6c52fb19ca195864a55e1818d9755ac4fd5dffb7
Johan Niklasson <johan.niklasson@klarna.com>
2021-02-01 13:17:53 +0100

    Added basic settings

---
+++ submission_filters.py
@@ -0,0 +1,8 @@
+SUBMISSION_MIN_SCORE = 2500
+"""
+The minimum score to consider in order to further consider process the submission.
+"""
+SUBMISSION_MAX_LENGTH = 30
+"""
+The maximum length of a video in order to further consider process the submission.
+"""
---
+++ topics.py
@@ -0,0 +1,8 @@
+PRE_PROCESS_TOPIC_ID: str = "pre-process-video-submission"
+"""
+The topic to push to in order to pre-process the video submission.
+"""
+PROCESS_TOPIC_ID: str = "process-video-submission"
+"""
+The topic to push to in order to process the video submission.
+"""
